a4875ce723daeea3baecca068880a1651246d7c2
kphed <kpcrypto69@gmail.com>
2022-07-19 13:18:13 -0400

    Add necessary external and interface contracts for calculating glpPrice and fee bps

---
+++ src/interface/IGlpManager.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+// https://arbiscan.io/address/0x321F653eED006AD1C29D174e17d96351BDe22649#code
+interface IGlpManager {
+    function getAums() external view returns (uint256[] memory);
+}
--- src/interface/IRewardRouterV2.sol
+++ src/interface/IRewardRouterV2.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-// RewardRouterV2.sol https://arbiscan.io/address/0xa906f338cb21815cbc4bc87ace9e68c87ef8d8f1#code
+// https://arbiscan.io/address/0xa906f338cb21815cbc4bc87ace9e68c87ef8d8f1#code
 interface IRewardRouterV2 {
     function mintAndStakeGlpETH(uint256 _minUsdg, uint256 _minGlp)
         external
