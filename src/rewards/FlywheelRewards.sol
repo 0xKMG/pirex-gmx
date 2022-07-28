09e8f24b3144b6a70731e2b774786a4376b9d94d
kphed <kpcrypto69@gmail.com>
2022-07-28 09:52:31 -0400

    Define fixed logic for reward accrual calculations

--- src/rewards/FlywheelRewards.sol
+++ src/rewards/FlywheelRewards.sol
@@ -14,25 +14,20 @@ import {FlywheelCore} from "./FlywheelCore.sol";
     - Modify code formatting and comment descriptions to be consistent with Pirex
     - Merge FlywheelRewards-related contract logic
     - Remove logic for supporting multiple strategies
+    - Remove modifier and unnecessary AccessControl base contract
+    - Define fixed logic for reward accrual calculations
 */
 contract FlywheelRewards {
     using SafeTransferLib for ERC20;
 
-    struct RewardsInfo {
-        // Rewards per second
-        uint224 rewardsPerSecond;
-        // The timestamp the rewards end at (0 = no end)
-        uint32 rewardsEndTimestamp;
-    }
+    // Amount of reward points to issue per second
+    uint256 public constant REWARDS_PER_SECOND = 1;
 
-    // The reward token paid
+    // Reward token
     ERC20 public immutable rewardToken;
 
-    // The flywheel core contract
+    // FlywheelCore contract
     FlywheelCore public immutable flywheel;
-
-    // Rewards info per strategy
-    RewardsInfo public rewardsInfo;
 
     error FlywheelError();
     error ZeroAddress();
@@ -45,43 +40,22 @@ contract FlywheelRewards {
 
         flywheel = _flywheel;
         rewardToken = _flywheel.rewardToken();
-        rewardsInfo = RewardsInfo({
-            rewardsPerSecond: 1,
-            rewardsEndTimestamp: 0
-        });
 
         rewardToken.safeApprove(address(_flywheel), type(uint256).max);
     }
 
-    modifier onlyFlywheel() {
-        if (msg.sender != address(flywheel)) revert FlywheelError();
-
-        _;
-    }
-
     /**
         @notice Calculate and transfer accrued rewards to flywheel core
-        @param  lastUpdatedTimestamp  uint32   The last updated time for strategy
-        @return amount                uint256  Amount of tokens accrued and transferred
+        @param  lastUpdatedTimestamp  uint256  The last updated time for strategy
+        @return                       uint256  Amount of tokens accrued and transferred
      */
-    function getAccruedRewards(uint32 lastUpdatedTimestamp)
+    function getAccruedRewards(uint256 lastUpdatedTimestamp)
         external
         view
-        onlyFlywheel
-        returns (uint256 amount)
+        returns (uint256)
     {
-        RewardsInfo memory rewards = rewardsInfo;
-        uint256 elapsed;
+        if (msg.sender != address(flywheel)) revert FlywheelError();
 
-        if (
-            rewards.rewardsEndTimestamp == 0 ||
-            rewards.rewardsEndTimestamp > block.timestamp
-        ) {
-            elapsed = block.timestamp - lastUpdatedTimestamp;
-        } else if (rewards.rewardsEndTimestamp > lastUpdatedTimestamp) {
-            elapsed = rewards.rewardsEndTimestamp - lastUpdatedTimestamp;
-        }
-
-        amount = rewards.rewardsPerSecond * elapsed;
+        return REWARDS_PER_SECOND * (block.timestamp - lastUpdatedTimestamp);
     }
 }
