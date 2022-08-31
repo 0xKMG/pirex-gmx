6699e6cbee791b44446097eb6c65a28a616e656b
kphed <kpcrypto69@gmail.com>
2022-08-31 13:23:54 -0400

    Add compound method for claiming pxGMX rewards

--- src/vaults/AutoPxGmx.sol
+++ src/vaults/AutoPxGmx.sol
@@ -3,10 +3,22 @@ pragma solidity 0.8.13;
 
 import {Owned} from "solmate/auth/Owned.sol";
 import {ERC4626} from "solmate/mixins/ERC4626.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
+import {PirexGmxGlp} from "src/PirexGmxGlp.sol";
 import {PirexRewards} from "src/PirexRewards.sol";
+import {IV3SwapRouter} from "src/interfaces/IV3SwapRouter.sol";
 
 contract AutoPxGmx is Owned, ERC4626 {
+    using SafeTransferLib for ERC20;
+
+    ERC20 public constant WETH =
+        ERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
+    ERC20 public constant GMX =
+        ERC20(0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a);
+    IV3SwapRouter public constant SWAP_ROUTER =
+        IV3SwapRouter(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45);
+
     uint256 public constant MAX_WITHDRAWAL_PENALTY = 500;
     uint256 public constant MAX_PLATFORM_FEE = 2000;
     uint256 public constant FEE_DENOMINATOR = 10000;
@@ -20,16 +32,28 @@ contract AutoPxGmx is Owned, ERC4626 {
     event PlatformFeeUpdated(uint256 fee);
     event PlatformUpdated(address _platform);
     event RewardsModuleUpdated(address _rewardsModule);
+    event Compounded(
+        address indexed caller,
+        uint24 fee,
+        uint256 amountOutMinimum,
+        uint160 sqrtPriceLimitX96,
+        uint256 wethAmountIn,
+        uint256 gmxAmountOut
+    );
 
     error ZeroAddress();
     error ExceedsMax();
     error AlreadySet();
+    error InvalidParam();
 
     constructor(address pxGmx)
         Owned(msg.sender)
         ERC4626(ERC20(pxGmx), "Autocompounding pxGMX", "apxGMX")
     {
         if (pxGmx == address(0)) revert ZeroAddress();
+
+        // Approve the Uniswap V3 router to manage our WETH (inbound swap token)
+        WETH.safeApprove(address(SWAP_ROUTER), type(uint256).max);
     }
 
     /**
@@ -86,5 +110,52 @@ contract AutoPxGmx is Owned, ERC4626 {
      */
     function totalAssets() public view override returns (uint256) {
         return asset.balanceOf(address(this));
+    }
+
+    /**
+        @notice Compound pxGMX rewards (privileged call to prevent manipulation)
+        @param  fee                uint24   Uniswap pool tier fee
+        @param  amountOutMinimum   uint256  Outbound token swap amount
+        @param  sqrtPriceLimitX96  uint160  Swap price impact limit
+     */
+    function compound(
+        uint24 fee,
+        uint256 amountOutMinimum,
+        uint160 sqrtPriceLimitX96
+    ) external onlyOwner {
+        if (fee == 0) revert InvalidParam();
+        if (amountOutMinimum == 0) revert InvalidParam();
+        if (sqrtPriceLimitX96 == 0) revert InvalidParam();
+
+        PirexRewards(rewardsModule).claim(asset, address(this));
+
+        // Swap entire WETH balance for GMX
+        uint256 wethAmountIn = WETH.balanceOf(address(this));
+        uint256 gmxAmountOut = SWAP_ROUTER.exactInputSingle(
+            IV3SwapRouter.ExactInputSingleParams({
+                tokenIn: address(WETH),
+                tokenOut: address(GMX),
+                fee: fee,
+                recipient: address(this),
+                amountIn: wethAmountIn,
+                amountOutMinimum: amountOutMinimum,
+                sqrtPriceLimitX96: sqrtPriceLimitX96
+            })
+        );
+
+        // Deposit entire GMX balance for pxGMX, increasing the asset/share amount
+        PirexGmxGlp(platform).depositGmx(
+            GMX.balanceOf(address(this)),
+            address(this)
+        );
+
+        emit Compounded(
+            msg.sender,
+            fee,
+            amountOutMinimum,
+            sqrtPriceLimitX96,
+            wethAmountIn,
+            gmxAmountOut
+        );
     }
 }
